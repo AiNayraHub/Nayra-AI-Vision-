2c6bd7a65aaf5317531f41d5f3ec32b9254ad9da
Aryan King <aryanking910926@gmail.com>
2026-02-02 18:02:33 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('nayra-v1').then((cache) => {
+      return cache.addAll(['index.html', 'privacy.html', 'terms.html']);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
