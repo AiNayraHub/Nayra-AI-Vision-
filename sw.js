6b6d9e7c1d9bd947c320b198ad745eae69e23b13
Aryan King <aryanking910926@gmail.com>
2026-02-02 18:18:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,41 @@
-self.addEventListener('fetch', function(event) {
-  // Service worker active rakhta hai app mode ke liye
+const cacheName = 'nayra-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './home.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
+
+self.addEventListener('fetch', async e => {
+  const req = e.request;
+  const url = new URL(req.url);
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const refresh = await fetch(req);
+    await cache.put(req, refresh.clone());
+    return refresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
