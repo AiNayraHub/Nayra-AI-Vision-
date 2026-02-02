68e1a5239eff04b719cfebdba75bc517bb491d80
Aryan King <aryanking910926@gmail.com>
2026-02-02 18:11:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,3 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('nayra-v1').then((cache) => {
-      return cache.addAll(['index.html', 'privacy.html', 'terms.html']);
-    })
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener('fetch', function(event) {
+  // Service worker active rakhta hai app mode ke liye
 });
