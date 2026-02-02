667a31d537d580d2d345eb98dc06a1ba10d8908f
Aryan King <aryanking910926@gmail.com>
2026-02-02 20:32:07 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'nayra-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './home.html',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
+
